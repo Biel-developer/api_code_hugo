fac77d25e5fd8d15a4f2f2148c7aafd097f6a4c1
Biel-developer <gabriel.naascimento18@gmail.com>
2026-05-10 21:33:34 -0300

    adicionando database

---
+++ database.py
@@ -0,0 +1,32 @@
+from sqlalchemy import create_engine, Column, Integer, String, Float
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+ 
+DATABASE_URL = "sqlite:///./jogos.db"
+ 
+engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+ 
+ 
+class JogoDB(Base):
+    __tablename__ = "jogos"
+ 
+    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    nome = Column(String, nullable=False)
+    tipo = Column(String, nullable=False)
+    nota = Column(Float, nullable=False)
+    review = Column(String, nullable=False)
+ 
+ 
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+ 
+ 
+def init_db():
+    Base.metadata.create_all(bind=engine)
+ 
